a3a1815d60d8ae4e9ffbe9bf75d04b40dfbae1a1
JoeFlake <flakejoe8@gmail.com>
2024-01-17 21:17:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiceSimulator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        // Get number of rolls
+        System.Console.Write("How many dice rolls would you like to simulate? ");
+        int numRolls = int.Parse(System.Console.ReadLine());
+
+        // Create the dice rolling instance
+        RollDie roll = new RollDie();
+        int[] results = roll.DieRoller(numRolls);
+
+        // Print the histogram
+        System.Console.WriteLine($"\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = {numRolls}.\n");
+        
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = (results[i] * 100) / numRolls;
+            string asterisks = new string('*', Math.Max(1, percentage));
+            Console.WriteLine($"{i}: {asterisks}");
+        }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+        
+    }
+}
---
+++ RollDie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceSimulator
+{
+    internal class RollDie
+    {
+        Random rnd = new Random();
+        public int[] DieRoller(int numRolls) 
+        {
+            // create array for roll possibilities
+            int[] aRolls = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < numRolls; i++)
+             {
+                int die1 = rnd.Next(1, 7);
+                int die2 = rnd.Next(1, 7);
+                int sum = die1 + die2;
+
+                aRolls[sum]++;
+             }
+            return aRolls;
+        }
+    }
+}
